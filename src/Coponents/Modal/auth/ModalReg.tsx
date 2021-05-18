fa8bc79162980d7f204a8ba6b9ff55b0916190cd
Tarxio <ilya.Tarxov@yandex.ru>
2021-05-18 11:25:01 +0300

    Вынес авторизацию/регистрацию

--- src/Coponents/Modal/auth/ModalReg.tsx
+++ src/Coponents/Modal/auth/ModalReg.tsx
@@ -1,73 +1,71 @@
-import React, {useState} from "react";
-import styles from "../../styles.module.css";
-import {Button, Form} from "antd";
-import Input from "../../../utils/Input/Input";
+import React from "react";
 import {registration} from "../../../Actions/User";
+import {useFormik} from "formik";
+import * as Yup from "yup";
+import "./Auth-Reg.css";
 
 const ModalReg = () => {
-    const [username, setUsername] =useState("")
-    const [email, setEmail] =useState("")
-    const [password, setPassword] =useState("")
-    const [SecPassword, setSecPassword] =useState("")
 
-    <p className={styles.modal_text}>Регистрация</p>
-    <hr className={styles.hr}/>
-    <Form
-        {...layout}
-        name="basic"
-        initialValues={{remember: true}}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-    >
-        <Form.Item name={['username']} label="Имя пользователя" rules={[{required: true}]}>
-            <Input value={username} setValue={setUsername} type={username} placeholder={"Введите имя пользователя..."}/>
-        </Form.Item>
+    const formik = useFormik({
+        initialValues: {
+            Username: '',
+            Password: '',
+        },
+        validationSchema: Yup.object({
+            Username: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required('Required'),
+            Password: Yup.string()
+                .required('No password provided.')
+                .min(3, 'Password is too short - should be 8 chars minimum.')
+                .matches(/[a-zA-Z а-яА-Я]/, 'Password  can only contain Latin letters.'),
+            confirmPassword: Yup.string()
+                .required('No password provided.')
+                .oneOf([yup.ref("password"), null], "Passwords must match")
+        });
+        }),
+        onSubmit: (values, {setSubmitting}) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+             }, 400)
+        }
+    });
 
-        <Form.Item name={['email']} label="E-mail" rules={[{type: 'email', required: true}]}>
-            <Input value={email} setValue={setEmail} type={email} placeholder={"Введите e-mail..."}/>
-        </Form.Item>
+    return (
+        <>
+            <h1>Регистрация</h1>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="Username">Имя пользователя:</label>
+                <input
+                    id="Username"
+                    name="Username"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.Username}
+                />
+                {formik.touched.Username && formik.errors.Username ? (
+                    <div>{formik.errors.Username}</div>
+                ) : null}
 
-        <Form.Item
-            label="Пароль"
-            name="password"
-            rules={[{required: true, message: 'Please input your password!'}]}
-        >
-            <Input value={password} setValue={setPassword} type={password} placeholder={"Введите пароль..."}/>
-        </Form.Item>
-        <Form.Item
-            name="confirm"
-            label="Повторите пароль"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-                {
-                    required: true,
-                    message: 'Please confirm your password!',
-                },
-                ({getFieldValue}) => ({
-                    validator(_, value) {
-                        if (!value || getFieldValue('password') === value) {
-                            return Promise.resolve();
-                        }
-                        return Promise.reject(new Error('The two passwords that you entered do not match!'));
-                    },
-                }),
-            ]}
-        >
-            <Input value={SecPassword} setValue={setSecPassword} type={password} placeholder={"Повторите пароль..."}/>
-        </Form.Item>
+                <label htmlFor="Password">Пароль:</label>
+                <input
+                    id="Password"
+                    name="Password"
+                    type="Password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.Password}
+                />
+                {formik.touched.Password && formik.errors.Password ? (
+                    <div>{formik.errors.Password}</div>
+                ) : null}
 
-
-        <Form.Item {...tailLayout}>
-            <Button type="primary"
-                    htmlType="submit"
-                    onClick={() => registration(username, email, password)}
-            >
-                Зарегистрироваться
-            </Button>
-        </Form.Item>
-    </Form>
-
+                <button type="submit" onClick={() => registration(formik.values.Username, "Email", formik.values.Password)}>Зарегистрироваться</button>
+            </form>
+        </>
+    );
 
 }
 
